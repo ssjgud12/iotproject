5ec0be870f56c842ece6aa966198d2d9a1434c15
ssjgud12 <150245254+ssjgud12@users.noreply.github.com>
2025-03-24 16:42:37 +0000

    added web server to arduino
    functionality has not been tested yet

---
+++ homepage.h.ino
@@ -0,0 +1,248 @@
+String HomePagePart1 =F(R”===== body {
+    background: url('lock.jpg') no-repeat center center fixed;
+    background-size: cover;
+    margin: 0;
+    padding: 0;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+
+.content {
+    text-align: center;
+    padding: 20px;
+    background: rgba(255, 255, 255, 0.7); /* Semi-transparent white */
+    display: inline-block;
+    border-radius: 10px;
+    margin: 50px auto;
+}
+
+/* Table styling */
+table {
+    
+    background-color: pink;
+    width: 700px;
+    margin: auto;
+    border-collapse: collapse;
+    border: 2px solid white;
+}
+
+th, td {
+    text-align: center;
+    padding: 10px;
+    border: 1px solid white;
+}
+
+th {
+    background-color: grey;
+    color: white;
+}
+
+p {
+    font-size: larger;
+    color: blue;
+}
+  =====“);
+
+ String homePagePart2 = F(R”===== <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IOT Project</title>
+    
+    <link rel="stylesheet" href="style.css">
+
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <div class="content">
+        <p><b>Welcome to my website showcasing my home security system</b></p>
+
+        <table>
+            <tr>
+                <th>Time</th>
+                <th>Date</th>
+                <th>Distance (Cm)</th>
+                <th>Distance (Inch)</th>
+                <th>Alarm mode</th>
+            </tr>
+            <tr>
+                <td> =====“);
+
+String HomePagePart2  ("===== <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IOT Project</title>
+    
+    <link rel="stylesheet" href="style.css">
+
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <div class="content">
+        <p><b>Welcome to my website showcasing my home security system</b></p>
+
+        <table>
+            <tr>
+                <th>Time</th>
+                <th>Date</th>
+                <th>Distance (Cm)</th>
+                <th>Distance (Inch)</th>
+                <th>Alarm mode</th>
+            </tr>
+            <tr>
+                <td> ====");
+                String HomePagePart3("==== </td>
+                <td>10/03/25</td>
+                <td>1</td>
+                <td>1</td>
+                <td>On/Off</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html> ====");
+                String HomePagePart4("==== <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IOT Project</title>
+    
+    <link rel="stylesheet" href="style.css">
+
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <div class="content">
+        <p><b>Welcome to my website showcasing my home security system</b></p>
+
+        <table>
+            <tr>
+                <th>Time</th>
+                <th>Date</th>
+                <th>Distance (Cm)</th>
+                <th>Distance (Inch)</th>
+                <th>Alarm mode</th>
+            </tr>
+            <tr>
+                <td>12:00</td>
+                <td> ====");
+                String HomePagePart5("==== </td>
+                <td>1</td>
+                <td>1</td>
+                <td>On/Off</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html> ====");
+
+String HomePagePart6 ("====<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IOT Project</title>
+    
+    <link rel="stylesheet" href="style.css">
+
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <div class="content">
+        <p><b>Welcome to my website showcasing my home security system</b></p>
+
+        <table>
+            <tr>
+                <th>Time</th>
+                <th>Date</th>
+                <th>Distance (Cm)</th>
+                <th>Distance (Inch)</th>
+                <th>Alarm mode</th>
+            </tr>
+            <tr>
+                <td>12:00</td>
+                <td>10/03/25</td>
+                <td> ====");
+String HomePagePart7 ("====1</td>
+                <td>1</td>
+                <td>On/Off</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
+ ====");
+String HomePagePart8 ("==== <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IOT Project</title>
+    
+    <link rel="stylesheet" href="style.css">
+
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <div class="content">
+        <p><b>Welcome to my website showcasing my home security system</b></p>
+
+        <table>
+            <tr>
+                <th>Time</th>
+                <th>Date</th>
+                <th>Distance (Cm)</th>
+                <th>Distance (Inch)</th>
+                <th>Alarm mode</th>
+            </tr>
+            <tr>
+                <td>12:00</td>
+                <td>10/03/25</td>
+                <td>1</td>
+                <td>====");
+String HomePagePart9 ("==== </td>
+                <td>On/Off</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>====");
+String HomePagePart10 ("==== <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IOT Project</title>
+    
+    <link rel="stylesheet" href="style.css">
+
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <div class="content">
+        <p><b>Welcome to my website showcasing my home security system</b></p>
+
+        <table>
+            <tr>
+                <th>Time</th>
+                <th>Date</th>
+                <th>Distance (Cm)</th>
+                <th>Distance (Inch)</th>
+                <th>Alarm mode</th>
+            </tr>
+            <tr>
+                <td>12:00</td>
+                <td>10/03/25</td>
+                <td>1</td>
+                <td>1</td>
+                <td> ====");
+String HomePagePart11 ("==== </td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html> ====");
